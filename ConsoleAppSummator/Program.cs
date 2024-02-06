738cd3349419e679d3879120f57cd301c3f6ee3d
Tetiana Kokhan <ipz223_kto@student.ztu.edu.ua>
2024-02-06 20:45:02 +0200

    Створено консольний проєкт

---
+++ ConsoleAppSummator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSummator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            double a, b, sum;
+            bool ok;
+
+            do
+            {
+                Console.Write("Введіть перше число: ");
+                ok = double.TryParse(Console.ReadLine(), out a);
+                if (!ok)
+                    Console.WriteLine("Некоректне значення! Будь ласка повторіть введення ще раз!");
+            } while (!ok);
+            do
+            {
+                Console.Write("Введіть друге число: ");
+                ok = double.TryParse(Console.ReadLine(), out b);
+                if (!ok)
+                    Console.WriteLine("Некоректне значення! Будь ласка повторіть введення ще раз!");
+            } while (!ok);
+            sum = a + b;
+            Console.WriteLine($"Сума: {sum}");
+        }
+    }
+}
